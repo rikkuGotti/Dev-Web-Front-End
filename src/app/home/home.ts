271afe2e6f0ca9a8e7496f84410d66172acf6364
Gotti <enricopimentel2006@gmail.com>
2026-08-28 15:02:22 -0300

    Exemplos adicionados na Aula 02

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  nome: string = '';
+
+  contador: number = 0;
+    incrementar() {this.contador++;}
+    decrementar() {this.contador--;}
+
+    mostrar = false;
+
+    frutas: string[] = ['Maçã', 'Banana', 'Laranja'];
+    selecionada = '';
+
+    cor: string = 'blue';
+}
